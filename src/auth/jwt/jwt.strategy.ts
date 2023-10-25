4c3303e699b96992fa8b54aa80d439a868e733bd
nandan-7 <148844450+nandan-7@users.noreply.github.com>
2023-10-26 01:02:56 +0530

    Update jwt.strategy.ts

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'YOUR_SECRET_KEY',
+      secretOrKey: 'nandan@123',
     });
   }
 
